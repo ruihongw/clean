2dcb41a306f44f9e9703fc9e6959d0fbc4814987
wrh <wrh19950110@gmail.com>
2019-01-18 21:49:04 +0800

    Initial Commit

---
+++ clean/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  clean
+//
+//  Created by Ruihongw on 2019/1/18.
+//  Copyright © 2019 Ruihongw. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
