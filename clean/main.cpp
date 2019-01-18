0f86236d8209c7715661048086fe2e3573c4e9ca
wrh <wrh19950110@gmail.com>
2019-01-18 21:50:15 +0800

    first commit

--- clean/main.cpp
+++ clean/main.cpp
@@ -7,9 +7,10 @@
 //
 
 #include <iostream>
+#include <vector>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+int main(){
     return 0;
 }
