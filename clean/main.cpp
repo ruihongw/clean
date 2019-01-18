164bd19edd6a153d42f26a14660b7fa0d5c05a7a
wrh <wrh19950110@gmail.com>
2019-01-18 22:23:09 +0800

    test

--- clean/main.cpp
+++ clean/main.cpp
@@ -11,6 +11,41 @@
 using namespace std;
 
 
+class CalculateNumbers{
+public:
+    CalculateNumbers(int _num1,int _num2){
+        num1=_num1;
+        num2=_num2;
+    }
+    CalculateNumbers(int _num1){
+        num1=_num1;
+        num2=0;
+    }
+    
+    int Add_Nums(){
+        return num1+num2;
+    }
+    int Minus_Nums(){
+        return num1-num2;
+    }
+    int Mult_Nums(){
+        return num1*num2;
+    }
+    int Devi_Nums(){
+        return num1/num2;
+    }
+
+private:
+    int num1=0,num2=0;
+};
+
 int main(){
+    CalculateNumbers instance1(1,2);
+    CalculateNumbers* instance2=new CalculateNumbers(2);
+    cout<<instance1.Add_Nums()<<endl;
+    cout<<instance1.Minus_Nums()<<endl;
+    cout<<instance1.Mult_Nums()<<endl;
+    cout<<instance1.Devi_Nums()<<endl;
+    cout<<instance2->Add_Nums()<<endl;
     return 0;
 }
